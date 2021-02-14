bb75af2fc7d8758594028b52b026abafefb316ff
prabhu446 <40172129+prabhu446@users.noreply.github.com>
2021-02-14 13:39:42 -0800

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,5 +1,6 @@
 user_input = input("Please enter an input:")
-reverse = user_input[:: -1]
+# ::-1 will put the given string/number in reverse
+reverse = user_input[::-1]
 print(reverse)
 if reverse == user_input:
     print("Given input is Palindrome")
