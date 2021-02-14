5ac3ee50f8ad3c2423cd5caab3b67181d9cde738
prabhu446 <40172129+prabhu446@users.noreply.github.com>
2021-02-14 15:02:17 -0800

    Update buzz_fizz.py

--- buzz_fizz.py
+++ buzz_fizz.py
@@ -3,8 +3,6 @@
 # If a number is divisible by both 3 and 5, print buzzfizz
 # If a number is NOT divisible by 3 and 5 then print just the given number.
 
-# There is no need to write else or elseif as a contional statement after if statement, we can directly return the output if the condition is not met.
-# Below I used only if conditions 3 times and finally if none of the 3 if statements are met then finally return the result as elseif statement.
 
 def buzz_fizz(user_input):
     if user_input % 3 == 0 and user_input % 5 == 0:
@@ -16,4 +14,5 @@ def buzz_fizz(user_input):
     return user_input
 
 
-print(buzz_fizz(15))
+user_input = int(input("Enter a number:"))
+print(buzz_fizz(user_input))
